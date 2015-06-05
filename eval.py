1f5a3457d99140ff71837c3f76b84aed236e0315
Jhow <jhowliu@gmail.com>
2015-06-05 23:00:21 +0800

    Add evaluation

---
+++ eval.py
@@ -0,0 +1,12 @@
+import sys
+import numpy as np
+
+if __name__ == '__main__':
+    # Load answer
+    tmp = open(sys.argv[1]).readlines()
+    ans = np.array(sorted(map(lambda x: int(x.replace('\n', '')), tmp)[1:]))
+
+    tmp = open(sys.argv[2]).readlines();
+    result = np.array(map(lambda x: int(x.replace('\n', '')), tmp))
+
+    print "Error = " + str(sum(result-ans))
